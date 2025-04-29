866f435c2635d1fbbfd7f7400bf912ae621073a4
Michael Duarte <mpduarte@gmail.com>
2025-04-29 16:03:20 -0700

    Update Docker run alias to use relative config path and fix env file location

--- local_build_install.sh
+++ local_build_install.sh
@@ -85,9 +85,9 @@ SHELL_RC="$HOME/.bashrc"
 
 ARCH=$(uname -m) && \
     if [ "$ARCH" = "x86_64" ]; then \
-        ALIAS_CMD="alias bridgy-start='docker run --rm -it --gpus all -v $CONFIG_DIR:/config --env-file /config/.env -p 8443:8443 $IMAGE_NAME'"
+        ALIAS_CMD="alias bridgy-start='docker run --rm -it --gpus all -v ./config:/config -p 8443:8443  --env-file ./config/.env $IMAGE_NAME'"
     else \
-        ALIAS_CMD="alias bridgy-start='docker run --rm -it -v $CONFIG_DIR:/config --env-file /config/.env -p 8443:8443 --env-file ./.env $IMAGE_NAME'"
+        ALIAS_CMD="alias bridgy-start='docker run --rm -it -v ./config:/config -p 8443:8443  --env-file ./config/.env $IMAGE_NAME'"
     fi
 
 if ! grep -Fq "alias bridgy-start=" "$SHELL_RC"; then
